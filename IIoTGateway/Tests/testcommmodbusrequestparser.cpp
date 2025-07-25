7667192d6d9b50275e58a7b657690df614a12d8a
ailton.fidelix <ailton1626@gmail.com>
2025-07-25 18:42:16 -0300

    refactor: Update modbus request test to use parameters

--- IIoTGateway/Tests/testcommmodbusrequestparser.cpp
+++ IIoTGateway/Tests/testcommmodbusrequestparser.cpp
@@ -1,254 +1,27 @@
 #include "testcommmodbusrequestparser.h"
 #include "testutils.h"
 
+using commmodbus::CommModbusRequestParser;
 using commmodbus::RequestType;
-
-void
-TestModbusJsonParserReadOne::SetUp()
-{
-    const QByteArray data = TestUtils::readJsonFile(":/requests/readone.json");
-
-    m_parser = new CommModbusRequestParser(data);
-}
-
-void
-TestModbusJsonParserReadOne::TearDown()
-{
-    if (m_parser)
-    {
-        delete m_parser;
-    }
-}
-
-TEST_F(TestModbusJsonParserReadOne, TestReadRequest)
-{
-    const auto readRequest = m_parser->request();
-    const auto type = m_parser->type();
-
-    ASSERT_FALSE(readRequest.isEmpty());
-    ASSERT_EQ(RequestType::Read, type);
-
-    auto addresses = readRequest.keys();
-    ASSERT_EQ(1, addresses.count());
-
-    auto address = addresses.first();
-    ASSERT_EQ(240, address);
-
-    auto unit = readRequest.value(address).at(0);
-
-    ASSERT_TRUE(unit.isValid());
-    ASSERT_EQ(QModbusDataUnit::HoldingRegisters, unit.registerType());
-    ASSERT_EQ(0, unit.startAddress());
-}
-
-void
-TestModbusJsonParserReadMultiple::SetUp()
-{
-    const QByteArray data = TestUtils::readJsonFile(":/requests/readmultiple.json");
-
-    m_parser = new CommModbusRequestParser{data};
-}
-
-void
-TestModbusJsonParserReadMultiple::TearDown()
-{
-    if (m_parser)
-    {
-        delete m_parser;
-    }
-}
-
-TEST_F(TestModbusJsonParserReadMultiple, TestReadRequest)
-{
-    auto testUnit = [](auto unit) -> void
-    {
-        ASSERT_TRUE(unit.isValid());
-        ASSERT_EQ(QModbusDataUnit::HoldingRegisters, unit.registerType());
-        ASSERT_EQ(10, unit.valueCount());
-    };
-
-    const auto readRequest = m_parser->request();
-    const auto type = m_parser->type();
-
-    ASSERT_FALSE(readRequest.isEmpty());
-    ASSERT_EQ(RequestType::Read, type);
-
-    auto addresses = readRequest.keys();
-    ASSERT_EQ(2, addresses.count());
-
-    const auto units1 = readRequest.value(1);
-    ASSERT_EQ(2, units1.count());
-
-    for (const auto &unit : units1)
-    {
-        testUnit(unit);
-    }
-
-    const auto units2 = readRequest.value(2);
-    ASSERT_EQ(4, units2.count());
-
-    for (const auto &unit : units2)
-    {
-        testUnit(unit);
-    }
-}
-
-void
-TestModbusJsonParserWriteOne::SetUp()
-{
-    const QByteArray data = TestUtils::readJsonFile(":/requests/writeone.json");
-
-    m_parser = new CommModbusRequestParser(data);
-}
-
-void
-TestModbusJsonParserWriteOne::TearDown()
-{
-    if (m_parser)
-    {
-        delete m_parser;
-    }
-}
-
-TEST_F(TestModbusJsonParserWriteOne, TestWriteRequest)
-{
-    const auto writeRequest = m_parser->request();
-    const auto type = m_parser->type();
-
-    ASSERT_FALSE(writeRequest.isEmpty());
-    ASSERT_EQ(RequestType::Write, type);
-
-    auto addresses = writeRequest.keys();
-    ASSERT_EQ(1, addresses.count());
-
-    auto address = addresses.first();
-    ASSERT_EQ(55, address);
-
-    auto unit = writeRequest.value(address).at(0);
-
-    ASSERT_TRUE(unit.isValid());
-    ASSERT_EQ(QModbusDataUnit::HoldingRegisters, unit.registerType());
-    ASSERT_EQ(3, unit.startAddress());
-
-    QList<uint16_t> expected = { 88, 2, 65533, 4, 5, 0, 7, 8, 255, 10 };
-    auto values = unit.values();
-
-    bool result = std::equal(values.begin(), values.end(), expected.begin());
-
-    ASSERT_TRUE(result);
-}
-
-void
-TestModbusJsonParserWriteMultiple::SetUp()
-{
-    const QByteArray data = TestUtils::readJsonFile(":/requests/writemultiple.json");
-
-    m_parser = new CommModbusRequestParser(data);
-}
-
-void
-TestModbusJsonParserWriteMultiple::TearDown()
-{
-    if (m_parser)
-    {
-        delete m_parser;
-    }
-}
-
-TEST_F(TestModbusJsonParserWriteMultiple, TestWriteRequest)
-{
-    auto testUnit = [] (auto type, auto start, auto &expected, auto &unit) -> void
-    {
-        ASSERT_TRUE(unit.isValid());
-        ASSERT_EQ(type, unit.registerType());
-        ASSERT_EQ(start, unit.startAddress());
-
-        auto values = unit.values();
-
-        ASSERT_EQ(expected.count(), values.count());
-
-        bool result = std::equal(values.begin(), values.end(), expected.begin());
-
-        ASSERT_TRUE(result);
-    };
-
-    const auto writeRequest = m_parser->request();
-    const auto type = m_parser->type();
-
-    ASSERT_FALSE(writeRequest.isEmpty());
-    ASSERT_EQ(RequestType::Write, type);
-
-    auto addresses = CommModbusRequestParser::sortedAddress(writeRequest);
-    ASSERT_EQ(2, addresses.count());
-
-    const QList<quint8> expectedAddresses{ 55, 60 };
-
-    ASSERT_EQ(expectedAddresses.count(), addresses.count());
-
-    bool result = std::equal(addresses.begin(), addresses.end(), expectedAddresses.begin());
-
-    ASSERT_TRUE(result);
-
-    auto unit1 = writeRequest.value(addresses.at(0));
-
-    ASSERT_EQ(1, unit1.count());
-
-    QList<quint16> expected{ 88, 2, 65533, 4, 5, 0, 7, 8, 255, 10 };
-
-    testUnit(QModbusDataUnit::HoldingRegisters, 3, expected, unit1.at(0));
-
-    auto unit2 = writeRequest.value(addresses.at(1));
-
-    ASSERT_EQ(2, unit2.count());
-
-    expected = { 88, 2, 65533, 4, 55, 0, 7, 8, 255, 1080 };
-
-    testUnit(QModbusDataUnit::HoldingRegisters, 0, expected, unit2.at(0));
-
-    expected = { 68, 72, 780, 2556, 101, 99, 0, 23 };
-
-    testUnit(QModbusDataUnit::HoldingRegisters, 10, expected, unit2.at(1));
-}
-
-void
-TestModbusJsonParserEmpty::SetUp()
-{
-    m_parser = new CommModbusRequestParser(QByteArray());
-}
-
-void
-TestModbusJsonParserEmpty::TearDown()
-{
-    if (m_parser)
-    {
-        delete m_parser;
-    }
-}
-
-TEST_F(TestModbusJsonParserEmpty, TestRequestEmpty)
-{
-    auto readRequest = m_parser->request();
-    auto type = m_parser->type();
-
-    ASSERT_EQ(RequestType::Unknown, type);
-    ASSERT_TRUE(readRequest.isEmpty());
-}
+using commmodbus::Units;
 
 TEST_P(TestModbusRequestParser, TestRequestParser)
 {
-    auto testUnit = [] (const QModbusDataUnit &expected, const QModbusDataUnit &actual) -> void
+    auto testUnit = [] (const QModbusDataUnit &expected, const QModbusDataUnit &actual) -> bool
     {
-        // ASSERT_TRUE(unit.isValid());
-        // ASSERT_EQ(type, unit.registerType());
-        // ASSERT_EQ(start, unit.startAddress());
+        bool result = (expected.registerType() == actual.registerType());
+        result &= (expected.startAddress() == actual.startAddress());
 
-        // auto values = unit.values();
+        const auto expectedValues = expected.values();
+        const auto actualValues = actual.values();
 
-        // ASSERT_EQ(expected.count(), values.count());
+        result &= (expectedValues.count() == actualValues.count());
 
-        // bool result = std::equal(values.begin(), values.end(), expected.begin());
+        result &= std::equal(actualValues.cbegin(), actualValues.cend(), expectedValues.cbegin(), [](const int a, const int b) {
+            return (a == b);
+        });
 
-        // ASSERT_TRUE(result);
+        return result;
     };
 
     const auto param = GetParam();
@@ -262,26 +35,27 @@ TEST_P(TestModbusRequestParser, TestRequestParser)
 
     CommModbusRequestParser parser(data);
 
-    const Request request = parser.request();
-    const RequestType type = parser.type();
+    const Request actualRequest = parser.request();
 
-    ASSERT_EQ(expectedRequest->count(), request.count());
+    ASSERT_EQ(expectedRequest->count(), actualRequest.count());
 
-    const auto keys = request.keys();
+    const auto keys = actualRequest.keys();
 
     for (const quint8 &key : keys)
     {
         ASSERT_TRUE(expectedRequest->contains(key));
 
         const Units expectedUnits = expectedRequest->value(key);
-        const Units currentUnits = request.value(key);
+        const Units actualUnits = actualRequest.value(key);
 
-        ASSERT_EQ(expectedUnits.count(), currentUnits.count());
+        ASSERT_EQ(expectedUnits.count(), actualUnits.count());
 
-        const bool result = std::equal(expectedUnits.cbegin(), expectedUnits.cend(), currentUnits.cbegin(), testUnit);
+        const bool result = std::equal(expectedUnits.cbegin(), expectedUnits.cend(), actualUnits.cbegin(), testUnit);
 
-        ASSERT_TRUE(result);
+        ASSERT_TRUE(result) << "Units are not equal";
     }
+
+    delete expectedRequest;
 }
 
 std::vector<std::tuple<QByteArray, Request*>> TestModbusRequestParser::LoadTestCases()
@@ -289,12 +63,52 @@ std::vector<std::tuple<QByteArray, Request*>> TestModbusRequestParser::LoadTestC
     std::vector<std::tuple<QByteArray, Request*>> cases{};
 
     {
+        // Test empty
+        auto request = new Request();
+        cases.push_back(std::make_tuple("", request));
+    }
+
+    {
+        // Test read one
         auto request = new Request();
 
-        QModbusDataUnit dataUnit(QModbusDataUnit::HoldingRegisters, 3, 10);
-        dataUnit.setValues({ 88, 2, 65533, 4, 5, 0, 7, 8, 255, 10 });
+        request->insert(240, Units({QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 0, 10)}));
 
-        request->insert(55, Units({dataUnit}));
+        cases.push_back(std::make_tuple(":/requests/readone.json", request));
+    }
+
+    {
+        // Test read multiple
+        auto request = new Request();
+
+        request->insert(1, Units({QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 0, 10),
+                                  QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 10, 10)}));
+
+        request->insert(2, Units({QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 0, 10),
+                                  QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 10, 10),
+                                  QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 20, 10),
+                                  QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 30, 10)}));
+
+        cases.push_back(std::make_tuple(":/requests/readmultiple.json", request));
+    }
+
+    {
+        // Test write one
+        auto request = new Request();
+
+        request->insert(55, Units({QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 3, { 88, 2, 65533, 4, 5, 0, 7, 8, 255, 10 })}));
+
+        cases.push_back(std::make_tuple(":/requests/writeone.json", request));
+    }
+
+    {
+        // Test write multiple
+        auto request = new Request();
+
+        request->insert(55, Units({QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 3, { 88, 2, 65533, 4, 5, 0, 7, 8, 255, 10 })}));
+
+        request->insert(60, Units({QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 0, { 88, 2, 65533, 4, 55, 0, 7, 8, 255, 1080 }),
+                                   QModbusDataUnit(QModbusDataUnit::HoldingRegisters, 10, { 68, 72, 780, 2556, 101, 99, 0, 23 })}));
 
         cases.push_back(std::make_tuple(":/requests/writemultiple.json", request));
     }
--- IIoTGateway/Tests/testcommmodbusrequestparser.h
+++ IIoTGateway/Tests/testcommmodbusrequestparser.h
@@ -4,65 +4,12 @@
 #include <gtest/gtest.h>
 #include "modbus/commmodbusrequestparser.h"
 
-using commmodbus::CommModbusRequestParser;
 using commmodbus::Request;
-using commmodbus::Units;
-
-class TestModbusJsonParserReadOne : public testing::Test
-{
-protected:
-    CommModbusRequestParser *m_parser;
-
-    void SetUp() override;
-    void TearDown() override;
-};
-
-class TestModbusJsonParserReadMultiple : public testing::Test
-{
-protected:
-    CommModbusRequestParser *m_parser;
-
-    void SetUp() override;
-    void TearDown() override;
-};
-
-class TestModbusJsonParserWriteOne : public testing::Test
-{
-protected:
-    CommModbusRequestParser *m_parser;
-
-    void SetUp() override;
-    void TearDown() override;
-};
-
-class TestModbusJsonParserWriteMultiple : public testing::Test
-{
-protected:
-    CommModbusRequestParser *m_parser;
-
-    void SetUp() override;
-    void TearDown() override;
-};
-
-class TestModbusJsonParserEmpty : public testing::Test
-{
-protected:
-    CommModbusRequestParser *m_parser;
-
-    void SetUp() override;
-    void TearDown() override;
-};
 
 class TestModbusRequestParser : public testing::TestWithParam<std::tuple<QByteArray, Request*>>
 {
 public:
     static std::vector<std::tuple<QByteArray, Request*>> LoadTestCases() ;
-protected:
-    CommModbusRequestParser *m_parser;
-
-
-    // void SetUp() override;
-    // void TearDown() override;
 };
 
 #endif // TESTCOMMMODBUSSETTINGSPARSER_H
