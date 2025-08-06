c72bd6c43f35be7ab7bdb0a9b2a479bcc40662f8
ailton.fidelix <ailton1626@gmail.com>
2025-08-06 20:18:55 -0300

    refactor: Remove ifdef

--- IIoTDashboard/src/main.cpp
+++ IIoTDashboard/src/main.cpp
@@ -6,9 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
     QGuiApplication app(argc, argv);
 
     QLocale::setDefault(QLocale::C);
