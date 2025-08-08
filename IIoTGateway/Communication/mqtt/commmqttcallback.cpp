a7ca0086d829b304a0736a9b5bdb4df8795758dc
ailton.fidelix <ailton1626@gmail.com>
2025-08-08 17:01:06 -0300

    refactor: Remove Q_FUNC_INFO macro

--- IIoTGateway/Communication/mqtt/commmqttcallback.cpp
+++ IIoTGateway/Communication/mqtt/commmqttcallback.cpp
@@ -6,13 +6,13 @@ COMM_MQTT_BEGIN_NAMESPACE
 
 void CommMQTTCallback::connected(const mqtt::string &message)
 {
-    qInfo() << Q_FUNC_INFO << "Connected:" << message;
+    qInfo() << "Connected:" << message;
     emit cbConnected();
 }
 
 void CommMQTTCallback::connection_lost(const std::string &cause)
 {
-    qWarning() << Q_FUNC_INFO << "Cause:" << cause;
+    qWarning() << "Connection lost:" << cause;
     emit cbConnectionLost();
 }
 
@@ -20,7 +20,7 @@ void CommMQTTCallback::message_arrived(mqtt::const_message_ptr ptr)
 {
     const auto message = QByteArray::fromStdString(ptr->to_string());
 
-    qInfo() << Q_FUNC_INFO << "Message:" << message;
+    qInfo() << "Message arrived:" << message;
 
     emit cbMessageArrived(message);
 }
@@ -28,8 +28,6 @@ void CommMQTTCallback::message_arrived(mqtt::const_message_ptr ptr)
 void CommMQTTCallback::delivery_complete(mqtt::delivery_token_ptr token)
 {
     Q_UNUSED(token)
-
-    qInfo() << Q_FUNC_INFO;
 
     emit cbDeliveryComplete();
 }
