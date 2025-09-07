915d9754ac5fe8cb0f067e8c5f688ff6ffb424b3
ailton.fidelix <ailton1626@gmail.com>
2025-09-07 14:52:29 -0300

    refactor: Adjust cmake and includes

--- IIoTGateway/Device/driver/driver_global.h
+++ IIoTGateway/Device/driver/driver_global.h
@@ -1,7 +1,7 @@
 #ifndef DRIVER_GLOBAL_H
 #define DRIVER_GLOBAL_H
 
-#include "../device_global.h"
+#include "device_global.h"
 
 #define DRIVER_BEGIN_NAMESPACE DEVICE_BEGIN_NAMESPACE namespace driver {
 #define DRIVER_END_NAMESPACE DEVICE_END_NAMESPACE }
