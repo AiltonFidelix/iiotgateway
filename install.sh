625eb2f086752371c342f5e04a575d240f17dbcd
ailton.fidelix <ailton1626@gmail.com>
2025-10-26 14:38:18 -0300

    feat: Update install script

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ echo "Installing dependencies..."
 apt-get update && apt-get --fix-broken install
 
 apt-get install -y \
+    python3-smbus \
+    i2c-tools \
     nginx \
     sqlite3 \
     libqt6sql6-sqlite
@@ -22,3 +24,8 @@ if [ ${#INSTALLERS[@]} -gt 0 ]; then
 else
     echo "No installers found."
 fi
+
+# --- Manual configurations ---
+# 1 - sudo raspi-config -> Interface Options -> Serial Port Enable hardware serial
+# 2 - sudo raspi-config -> Interface Options -> I2C -> Enable hardware I2C
+# 3 - sudo nano /boot/firmware/config.txt -> Add dtoverlay=i2c-rtc,ds1307
