46c51b97bdbdd67b21d224d8df4f2bd27ca91f8c
ailton.fidelix <ailton1626@gmail.com>
2026-08-16 21:20:50 -0300

    refactor: Format file, adjust constants and fix connection crash

--- gateway/Communication/mqtt/commmqtt.cpp
+++ gateway/Communication/mqtt/commmqtt.cpp
@@ -1,32 +1,29 @@
-#include "commfactory.hpp"
 #include "commmqtt.hpp"
 
 #include <QDebug>
 #include <QJsonDocument>
 
-namespace
-{
-    constexpr int defaultQos = 0;
-    constexpr int defaultConnectionTimeout = 10;
-    constexpr int defaultKeepAliveInterval = 60;
-    constexpr int maxRetries = 5;
+#include "commfactory.hpp"
+
+namespace {
+    constexpr int DEFAULT_QOS = 0;
+    constexpr int MAX_RETRIES = 5;
 }
 
 COMM_MQTT_BEGIN_NAMESPACE
 
-int CommMQTT::m_typeId = comm::CommFactory::registerInterface<CommMQTT*>("MQTT");
+int CommMQTT::m_typeId = comm::CommFactory::registerInterface<CommMQTT *>("MQTT");
 
-CommMQTT::CommMQTT(QJsonObject settings) :
-    m_pubQos{defaultQos},
-    m_subQos{defaultQos},
-    m_retries{0},
-    m_publish{false},
-    m_subscribe{false},
-    m_client{nullptr},
-    m_pubTopic{},
-    m_subTopic{},
-    m_settingsParser{std::move(settings)}
-{
+CommMQTT::CommMQTT(QJsonObject settings)
+    : m_pubQos{DEFAULT_QOS},
+      m_subQos{DEFAULT_QOS},
+      m_retries{0},
+      m_publish{false},
+      m_subscribe{false},
+      m_client{nullptr},
+      m_pubTopic{},
+      m_subTopic{},
+      m_settingsParser{std::move(settings)} {
     m_publish = m_settingsParser.publish();
     m_subscribe = m_settingsParser.subscribe();
     m_pubTopic = m_settingsParser.publishTopic();
@@ -38,48 +35,41 @@ CommMQTT::CommMQTT(QJsonObject settings) :
     connect(&m_callback, &CommMQTTCallback::cbMessageArrived, this, &CommMQTT::onMessageArrived);
 }
 
-CommMQTT::~CommMQTT()
-{
-    if (m_client)
-    {
+CommMQTT::~CommMQTT() {
+    if (m_client) {
         delete m_client;
     }
 }
 
-bool CommMQTT::isconnected() const
-{
+bool CommMQTT::isconnected() const {
     return m_client->is_connected();
 }
 
-void CommMQTT::setMQTTClient(mqtt::iasync_client *client)
-{
+void CommMQTT::setMQTTClient(mqtt::iasync_client *client) {
     m_client = client;
 }
 
-void CommMQTT::connectComm()
-{
-    if (m_client == nullptr)
-    {
+void CommMQTT::connectComm() {
+    if (m_client == nullptr) {
         const std::string protocol = m_settingsParser.protocol();
-        const std::string  host = m_settingsParser.host();
+        const std::string host = m_settingsParser.host();
         const int port = m_settingsParser.port();
 
-        const std::string server_address = QString("%1://%2%3%4").arg(QString::fromStdString(protocol),
-                                                                      QString::fromStdString(host),
-                                                                      (port == -1) ? QString() : QString(":"),
-                                                                      QString::number(port)).toStdString();
+        const std::string server_address = QString("%1://%2%3%4").arg(QString::fromStdString(protocol), QString::fromStdString(host), (port == -1) ? QString() : QString(":"), QString::number(port)).toStdString();
 
         const std::string client_id = m_settingsParser.clientId();
 
-        m_client = new mqtt::async_client(server_address, client_id);
-
-        m_client->set_callback(m_callback);
+        try {
+            m_client = new mqtt::async_client(server_address, client_id);
+            m_client->set_callback(m_callback);
+        } catch (const mqtt::exception &e) {
+            qCritical() << "MQTT connection exception:" << e.what();
+            return emit connectionFailed();
+        }
     }
 
-    if (m_retries++ >= maxRetries)
-    {
-        emit connectionFailed();
-        return;
+    if (m_retries++ >= MAX_RETRIES) {
+        return emit connectionFailed();
     }
 
     mqtt::connect_options connOpts{};
@@ -87,8 +77,7 @@ void CommMQTT::connectComm()
     const std::string username = m_settingsParser.username();
     const std::string password = m_settingsParser.password();
 
-    if (!username.empty() && !password.empty())
-    {
+    if (!username.empty() && !password.empty()) {
         connOpts.set_user_name(username);
         connOpts.set_password(password);
     }
@@ -102,32 +91,27 @@ void CommMQTT::connectComm()
 
     connect(&m_listener, &CommMQTTActionListener::connectionFailed, this, &CommMQTT::connectComm);
 
-    try
-    {
+    try {
         qInfo() << "Connecting...";
         auto connTok = m_client->connect(connOpts, nullptr, m_listener);
         qInfo() << "Waiting for the connection...";
 
-        if (connTok)
-        {
+        if (connTok) {
             connTok->wait();
             qInfo() << "Successfully connected";
+
+            emit connected();
         }
-    }
-    catch (const mqtt::exception& ex)
-    {
-        emit error(QString("MQTT connection exception: %1").arg(ex.what()).toUtf8());
+    } catch (const mqtt::exception &e) {
+        qCritical() << "MQTT connection exception:" << e.what();
     }
 }
 
-void CommMQTT::disconnectComm()
-{
-    if (isconnected())
-    {
+void CommMQTT::disconnectComm() {
+    if (isconnected()) {
         auto discTok = m_client->disconnect();
 
-        if (discTok)
-        {
+        if (discTok) {
             discTok->wait();
         }
     }
@@ -135,49 +119,39 @@ void CommMQTT::disconnectComm()
     emit disconnected();
 }
 
-void CommMQTT::incoming(QByteArray data)
-{
-    if (!isconnected())
-    {
+void CommMQTT::incoming(QByteArray data) {
+    if (!isconnected()) {
         qWarning() << "MQTT publish failed: Not connected!";
         return;
     }
 
-    if (!m_publish)
-    {
+    if (!m_publish) {
         qWarning() << "MQTT publish failed: Publish is not configured!";
         return;
     }
 
     const auto message = data.toStdString();
 
-    try
-    {
+    try {
         auto pubMsg = mqtt::make_message(m_pubTopic, message, m_pubQos, false);
         auto delTok = m_client->publish(pubMsg);
 
-        if (delTok)
-        {
+        if (delTok) {
             delTok->wait();
         }
-    }
-    catch (const mqtt::exception& ex)
-    {
+    } catch (const mqtt::exception &ex) {
         emit error(QString("MQTT publish exception: %1").arg(ex.what()).toUtf8());
     }
 }
 
-void CommMQTT::onConnected()
-{
-    if (m_subscribe)
-    {
+void CommMQTT::onConnected() {
+    if (m_subscribe) {
         m_client->subscribe(m_subTopic, m_subQos);
         qDebug() << "MQTT topic subscribed:" << m_subTopic;
     }
 }
 
-void CommMQTT::onMessageArrived(QByteArray message)
-{
+void CommMQTT::onMessageArrived(QByteArray message) {
     emit outgoing(std::move(message));
 }
 
