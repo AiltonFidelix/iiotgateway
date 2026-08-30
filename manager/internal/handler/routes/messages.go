368725da302635c5fd51f8bfc80f42aa9c586891
ailton.fidelix <ailton1626@gmail.com>
2026-08-30 18:53:41 -0300

    feat: Create constant messages

---
+++ manager/internal/handler/routes/messages.go
@@ -0,0 +1,8 @@
+package routes
+
+const (
+	INTERNAL_ERROR_MESSAGE  = "Internal error"
+	STATUS_FAILED_MESSAGE   = "Status failed:"
+	LOGIN_FAILED_MESSAGE    = "Login failed:"
+	RESPONSE_FAILED_MESSAGE = "Failed to write response:"
+)
