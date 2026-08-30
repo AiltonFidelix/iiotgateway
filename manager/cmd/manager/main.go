d483bb42dba2e72e3a7752045ca20ea106b3733b
ailton.fidelix <ailton1626@gmail.com>
2026-08-30 18:56:24 -0300

    feat: Enable cors and add repositories to the handler

--- manager/cmd/manager/main.go
+++ manager/cmd/manager/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	"manager/internal/config"
 	"manager/internal/handler"
+	"manager/internal/repository"
 
 	_ "modernc.org/sqlite"
 )
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	db, err := sql.Open(config.DB_TYPE, config.DB_NAME)
@@ -20,11 +38,18 @@ func main() {
 
 	defer db.Close()
 
+	userRepository := repository.NewUserRepository(db)
+	protocolRepository := repository.NewProtocolRepository(db)
+
 	managerHandler := handler.NewManagerHandler()
+	managerHandler.SetUserRepository(userRepository)
+	managerHandler.SetProtocolRepository(protocolRepository)
 
 	mux := http.NewServeMux()
 	managerHandler.RegisterRoutes(mux)
 
-	log.Printf("Starting manager version %v on port :%v\n", config.VERSION, config.SERVER_PORT)
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	portStr := fmt.Sprintf(":%d", config.SERVER_PORT)
+
+	log.Printf("Starting manager version %v on port %v\n", config.VERSION, portStr)
+	log.Fatal(http.ListenAndServe(portStr, enableCORS(mux)))
 }
