d3079928ff7b2e00daaa9b6a19743e3d71ba59da
ailton.fidelix <ailton1626@gmail.com>
2026-08-30 18:55:33 -0300

    feat: Adjust interfaces and models

--- manager/internal/domain/interfaces.go
+++ manager/internal/domain/interfaces.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+type ResponseModel struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 type HistoryModel struct {
 	ID      int
 	Script  string
@@ -30,30 +35,29 @@ type SettingsModel struct {
 
 type UserModel struct {
 	ID       int
-	UserName string
-	Password string
+	Name     string `json:"username"`
+	Password string `json:"password"`
 	Created  time.Time
 	Updated  time.Time
 }
 
 type HistoryRepository interface {
-	GetHistory(ctx context.Context) (*HistoryModel, error)
-	UpdateHistory(ctx context.Context, s *HistoryModel) error
+	InsertHistory(ctx context.Context, history *HistoryModel) error
 }
 
 type ProtocolRepository interface {
-	GetProtocol(ctx context.Context) (*ProtocolModel, error)
-	UpdateProtocol(ctx context.Context, s *ProtocolModel) error
+	GetProtocol(ctx context.Context, protocolType string) (*ProtocolModel, error)
+	InsertProtocol(ctx context.Context, protocol *ProtocolModel) error
+	UpdateProtocol(ctx context.Context, protocol *ProtocolModel) error
 }
 
 type SettingsRepository interface {
 	GetSettings(ctx context.Context) (*SettingsModel, error)
-	UpdateSettings(ctx context.Context, s *SettingsModel) error
+	UpdateSettings(ctx context.Context, settings *SettingsModel) error
 }
 
 type UserRepository interface {
 	GetUser(ctx context.Context) (*UserModel, error)
-	UpdateUser(ctx context.Context, s *UserModel) error
 }
 
 type SystemController interface {
