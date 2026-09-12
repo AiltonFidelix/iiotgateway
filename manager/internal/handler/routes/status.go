5d2868228efe3bde91f878a134f99b068ebfe21c
ailton.fidelix <ailton1626@gmail.com>
2026-09-11 22:06:54 -0300

    feat: Start implement status endpoint

--- manager/internal/handler/routes/status.go
+++ manager/internal/handler/routes/status.go
@@ -1,10 +1,40 @@
 package routes
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
+	"log"
+	"manager/internal/domain"
 	"net/http"
+	"time"
 )
 
-func GetStatus(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "GetStatus\n")
+func GetStatus(w http.ResponseWriter, req *http.Request, controller domain.SystemController) {
+
+	// TODO Remove comment
+	// if controller == nil {
+	// 	log.Println("Get status failed, controller not valid!")
+	// 	http.Error(w, INTERNAL_ERROR_MESSAGE, http.StatusInternalServerError)
+	// 	return
+	// }
+
+	ctx := req.Context()
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+	defer cancel()
+
+	respData := domain.ResponseModel{
+		Status:  "ok",      // error
+		Message: "Stopped", // Running
+	}
+
+	// TODO Read gateway status using the controller
+
+	err := json.NewEncoder(w).Encode(respData)
+
+	if err != nil {
+		log.Println(STATUS_FAILED_MESSAGE, err)
+		http.Error(w, INTERNAL_ERROR_MESSAGE, http.StatusInternalServerError)
+	}
 }
