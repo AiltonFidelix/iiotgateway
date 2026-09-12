f088c62615732b5342331f4c0a29d9079601946b
ailton.fidelix <ailton1626@gmail.com>
2026-09-11 22:07:18 -0300

    feat: Create commons for the endpoints

---
+++ manager/internal/handler/commons.go
@@ -0,0 +1,8 @@
+package handler
+
+const (
+	INTERNAL_ERROR_MESSAGE  = "Internal error"
+	STATUS_FAILED_MESSAGE   = "Status failed:"
+	LOGIN_FAILED_MESSAGE    = "Login failed:"
+	RESPONSE_FAILED_MESSAGE = "Failed to write response:"
+)
